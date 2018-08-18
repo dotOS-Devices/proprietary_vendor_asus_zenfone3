077b2ee8a3fc86bbd61511f732f6a1f42aa2fba7
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2018-08-18 19:26:54 +0700

    zenfone3: Cleanup and Update blobs from WW-15.0410.1807.72

--- zenfone3/Android.mk
+++ zenfone3/Android.mk
@@ -82,18 +82,6 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libaudio_log_utils
-LOCAL_MODULE_OWNER := asus
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libaudio_log_utils.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libaudio_log_utils.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libbtnv
 LOCAL_MODULE_OWNER := asus
 LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libbtnv.so
